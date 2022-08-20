b7aca0876314cd82dd8f50557d228fd375fb2f70
BryanHo99 <hoyungkynn1560@gmail.com>
2022-08-20 21:43:03 +0800

    Removed env check in main

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ from mao_bot import MaoBot
 
 
 def main():
-    if not isfile(".env"):
-        print("No '.env' file found.\nPlease make sure it is there.")
-
-    else:
-        bot = MaoBot(command_prefix=config.PREFIX)
-        bot.run(config.BOT_TOKEN)
+    bot = MaoBot(command_prefix=config.PREFIX)
+    bot.run(config.BOT_TOKEN)
 
 
 if __name__ == "__main__":
